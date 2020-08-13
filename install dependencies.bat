df5e16861534dc41753b523ddceabadc01adbba2
SpiderDave <ifallelsefailsusefire@gmail.com>
2020-08-13 19:00:29 -0400

    adding better error handling and detection of python

--- install dependencies.bat
+++ install dependencies.bat
@@ -1,22 +1,61 @@
 @echo off
-echo Checking for python...
+
+rem test to make sure we have the "where" command
+where/?>nul 2>&1
+if %errorlevel% NEQ 0 set errormessage="where" command not available.& goto error
+
+
+rem -- find python --------------------
+echo Locating python...
+echo Attempt 1: "py"
 py --version 2>NUL
 if errorlevel 1 set errormessage=Error: Could not find python & goto error
+set pycmd=py
+goto foundpython
+
+echo Attempt 2: "python"
+python -c "from sys import version_info as v;_=0/int(v.major/3)">nul 2>&1
+if %errorlevel% EQU 0 set pycmd=python&goto foundpython
+
+echo Attempt 3: "python" using "python" environment variable
+
+rem check for environment variable
+if %python%X EQU X goto skip1
+
+rem test "%python%/python"
+%python%\python -c "from sys import version_info as v;_=0/int(v.major/3)">nul 2>&1
+if %errorlevel% NEQ 0 goto skip1
+
+set pycmd=%python%\python
+goto foundpython
+
+:skip1
+
+:foundpython
+echo Found.
+
+rem -----------------------------------
+
 echo.
 
+rem echo Checking for python...
+rem py --version 2>NUL
+rem if errorlevel 1 set errormessage=Error: Could not find python & goto error
+rem echo.
+
 echo Checking for pip...
-py -m pip --version 2>NUL
+%pycmd% -m pip --version 2>NUL
 if errorlevel 1 set errormessage=Error: Could not find pip & goto error
 echo.
 
 echo Attempting to install lupa...
-py -m pip install lupa
+%pycmd% -m pip install lupa
 echo Attempting to install pyinstaller...
-py -m pip install pyinstaller
+%pycmd% -m pip install pyinstaller
 echo Attempting to install pillow...
-py -m pip install pillow
+%pycmd% -m pip install pillow
 echo Attempting to install numpy...
-py -m pip install numpy
+%pycmd% -m pip install numpy
 
 if %errorlevel% NEQ 0 goto error
 
